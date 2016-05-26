2b8cf937dd1e6edc1c2b2122da62ed865d43205e
Esteban <esteban.muruzabal@globant.com>
2016-05-26 12:33:31 -0300

    added categories module to client

---
+++ app/models/product.server.model.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+//validation length
+function validateLength(v){
+	return v.length <= 15;
+}
+/**
+ * Product Schema
+ */
+var ProductSchema = new Schema({
+	category: {
+		type: Schema.Types.ObjectId,
+		ref: 'Category',
+		required: 'invalid category'
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	name: {
+		type: String,
+		default: '',
+		trim: true,
+		required: 'name cannot be empty',
+		validate: [validateLength, 'name must be 40 chars in length or less']
+	},
+	quantityPerUnit: {
+		type: String
+	},
+	unitPrice: {
+		type: Number,
+		default: 0
+	},
+	unitsInStock: {
+		type: Number,
+		default: 0,
+		min: 0
+	},
+	unitsOnOrder: {
+		type: Number,
+		default: 0,
+		min: 0
+	},
+	discontinued: {
+		type: Boolean,
+		default: false
+	}
+
+});
+
+mongoose.model('Product', ProductSchema);
