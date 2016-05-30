8de6d84770eab5d24d8027bec727dc00a299ffbc
Esteban <esteban.muruzabal@globant.com>
2016-05-30 19:42:35 -0300

    added CRUD controller

--- app/routes/categories.server.routes.js
+++ app/routes/categories.server.routes.js
@@ -1,23 +1,19 @@
+'use strict';
 
-    'use strict';
+module.exports = function(app) {
+    var categories = require('../controllers/categories.server.controller');
+    var users = require('../controllers/users.server.controller');
+    var apiAuth = require('../controllers/api.authorization.server.controller');
 
-    module.exports = function(app) {
-        var categories = require('../../app/controllers/categories.server.controller');
-        var users = require('../controllers/users.server.controller');
-        var apiAuth = require('../controllers/api.authorization.server.controller');
+    app.route('/categories')
+        .get(apiAuth, users.requiresLogin, categories.list)
+        .post(apiAuth, users.requiresLogin, categories.create);
 
-        app.route('/categories')
-            .get(apiAuth, users.requiresLogin, categories.list)
-            .post(apiAuth, users.requiresLogin, categories.create);
+    app.route('/categories/:categoryId')
+        .get(apiAuth, users.requiresLogin, categories.read)
+        .put(apiAuth, users.requiresLogin, categories.update)
+        .delete(apiAuth, users.requiresLogin, categories.delete);
 
-        app.route('/categories/:categoryId')
-            .get(apiAuth, users.requiresLogin, categories.read)
-            .put( apiAuth, users.requiresLogin, categories.update)
-            .delete( apiAuth, users.requiresLogin, categories.delete);
-
-        // Finish by binding the article middleware
-        // What's this? Where the categoryId is present in the URL
-        // the logic to 'get by id' is handled by this single function
-        // and added to the request object i.e. request.category.
-        app.param('categoryId', categories.categoryByID);
-    };
+    // Finish by binding the article middleware
+    app.param('categoryId', categories.getByID);
+};
--- app/routes/products.server.routes.js
+++ app/routes/products.server.routes.js
@@ -1,22 +1,19 @@
 'use strict';
 
 module.exports = function(app) {
-	var products = require('../../app/controllers/products.server.controller');
-	var users = require('../../app/controllers/users.server.controller');
+	var products = require('../controllers/products.server.controller');
+	var users = require('../controllers/users.server.controller');
 	var apiAuth = require('../controllers/api.authorization.server.controller');
 
 	app.route('/products')
 		.get(apiAuth, users.requiresLogin, products.list)
 		.post(apiAuth, users.requiresLogin, products.create);
 
-	app.route('/products/:productsId')
+	app.route('/products/:productId')
 		.get(apiAuth, users.requiresLogin, products.read)
 		.put(apiAuth, users.requiresLogin, products.update)
 		.delete(apiAuth, users.requiresLogin, products.delete);
 
 	// Finish by binding the article middleware
-	// What's this? Where the categoryId is present in the URL
-	// the logic to 'get by id' is handled by this single function
-	// and added to the request object i.e. request.category.
-	app.param('productsId', products.productByID);
+	app.param('productId', products.getByID);
 };
